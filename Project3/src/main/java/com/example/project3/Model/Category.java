7a8ba9b3b32c115fb83f357f3ad61e5243e26dd0
Abdulmajeed <132199458+mj95d@users.noreply.github.com>
2023-05-22 20:02:45 +0300

    Category.java

--- Project3/src/main/java/com/example/project3/Model/Category.java
+++ Project3/src/main/java/com/example/project3/Model/Category.java
@@ -1,21 +1,25 @@
 package com.example.project3.Model;
 
+
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
+
+import java.util.ArrayList;
 
 @AllArgsConstructor
 @Data
 public class Category {
 
-    @NotEmpty(message = "id must not be empty")
-    @Length(min = 3, message = "id have to be 3 character long")
-    private String id;
+   @NotEmpty(message = "id must not be empty")
+   @Length(min = 3, message = "id have to be 3 character long")
+   private String id;
 
-    @NotEmpty(message = "name must not be empty")
-    @Length(min = 3, message = "name have to be 3 character long")
-    private String name;
+   @NotEmpty(message = "name must not be empty")
+   @Length(min = 3, message = "name have to be 3 character long")
+   private String name;
 
 }
-
