04379cf92f1a40e0b85a767045ba337476dfed62
Abdulmajeed <132199458+mj95d@users.noreply.github.com>
2023-05-22 18:07:10 +0300

    Product.java

--- Project3/src/main/java/com/example/project3/Model/Product.java
+++ Project3/src/main/java/com/example/project3/Model/Product.java
@@ -1,6 +1,7 @@
 package com.example.project3.Model;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
